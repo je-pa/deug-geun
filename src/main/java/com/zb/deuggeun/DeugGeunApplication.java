e642de4cdd2dce193cc65ab822a10d0e99e002ab
je-pa <jieun0502@gmail.com>
2024-03-21 19:57:58 +0900

    🎉 Begin a project

---
+++ src/main/java/com/zb/deuggeun/DeugGeunApplication.java
@@ -0,0 +1,13 @@
+package com.zb.deuggeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeugGeunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeugGeunApplication.class, args);
+    }
+
+}
