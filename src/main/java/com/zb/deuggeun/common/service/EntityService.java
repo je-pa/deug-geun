609c24ca08a4870d84c5181d769219d0afa7835a
je-pa <jieun0502@gmail.com>
2024-04-04 17:48:10 +0900

    ♻️ Refactor: EntityServiceUtil → interface로 빼기

--- src/main/java/com/zb/deuggeun/common/service/EntityService.java
+++ src/main/java/com/zb/deuggeun/common/service/EntityService.java
@@ -1,20 +1,17 @@
-package com.zb.deuggeun.common.util;
+package com.zb.deuggeun.common.service;
 
 import static com.zb.deuggeun.common.exception.ExceptionCode.ENTITY_NOT_FOUND;
 
 import com.zb.deuggeun.common.exception.CustomException;
-import lombok.experimental.UtilityClass;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@UtilityClass
-public class EntityServiceUtil {
+public interface EntityService {
 
-  public static <T, ID> T findById(JpaRepository<T, ID> repository, ID id) {
+  static <T, ID> T findById(JpaRepository<T, ID> repository, ID id) {
     return repository.findById(id)
         .orElseThrow(() -> new CustomException(
             ENTITY_NOT_FOUND.getStatus(),
             ENTITY_NOT_FOUND.getMessage()
         ));
   }
-
 }
