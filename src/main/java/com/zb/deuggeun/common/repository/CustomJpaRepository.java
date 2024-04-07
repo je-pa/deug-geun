2a5845db1980c5aed004cdc51c4e806cc4241f66
je-pa <jieun0502@gmail.com>
2024-04-07 23:17:03 +0900

    🐛 Fix a bug: CustomJpaRepository

--- src/main/java/com/zb/deuggeun/common/repository/CustomJpaRepository.java
+++ src/main/java/com/zb/deuggeun/common/repository/CustomJpaRepository.java
@@ -4,7 +4,9 @@ import static com.zb.deuggeun.common.exception.ExceptionCode.ENTITY_NOT_FOUND;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.zb.deuggeun.common.exception.CustomException;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface CustomJpaRepository<T, ID> extends JpaRepository<T, ID> {
 
   default T findByIdWithThrow(ID id) {
