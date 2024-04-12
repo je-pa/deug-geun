324f049ddf8c42d29435ea8fd7e998551f04fbdd
je-pa <jieun0502@gmail.com>
2024-04-12 22:21:32 +0900

    ⚡️Improve performance: 시간복잡도 개선

--- src/main/java/com/zb/deuggeun/programschedule/dto/TimeOverlapCheckable.java
+++ src/main/java/com/zb/deuggeun/programschedule/dto/TimeOverlapCheckable.java
@@ -1,6 +1,7 @@
 package com.zb.deuggeun.programschedule.dto;
 
 import com.zb.deuggeun.common.validation.type.TimeOrderValidatable;
+import java.util.Comparator;
 import java.util.List;
 
 public interface TimeOverlapCheckable<T extends TimeOrderValidatable> {
@@ -8,20 +9,19 @@ public interface TimeOverlapCheckable<T extends TimeOrderValidatable> {
   List<T> getTimeOrderValidatableList();
 
   default boolean hasOverlap() {
-    final int H = 24;
-    final int M = 60;
-    boolean[] timeArray = new boolean[H * M]; // 24시간 * 60분
     List<T> timeOrderValidatableList = getTimeOrderValidatableList();
 
-    for (TimeOrderValidatable timeSlot : timeOrderValidatableList) {
-      int startMinute =
-          timeSlot.getStartTime().getHour() * M + timeSlot.getStartTime().getMinute();
-      int endMinute = timeSlot.getEndTime().getHour() * M + timeSlot.getEndTime().getMinute();
-      for (int i = startMinute; i < endMinute; i++) {
-        if (timeArray[i]) {
-          return true; // 겹치는 시간대가 있으면 true 반환
-        }
-        timeArray[i] = true;
+    // 시간대를 시작 시간을 기준으로 정렬
+    timeOrderValidatableList.sort(Comparator.comparing(t -> t.getStartTime()));
+
+    // 시간대를 순회하면서 겹치는지 확인
+    for (int i = 0; i < timeOrderValidatableList.size() - 1; i++) {
+      T firstTimeSlot = timeOrderValidatableList.get(i);
+      T secondTimeSlot = timeOrderValidatableList.get(i + 1);
+
+      // 첫 번째 시간대의 종료 시간과 두 번째 시간대의 시작 시간을 비교하여 겹치는지 확인
+      if (firstTimeSlot.getEndTime().isAfter(secondTimeSlot.getStartTime())) {
+        return true; // 겹치는 시간대가 있으면 true 반환
       }
     }
     return false; // 겹치는 시간대가 없으면 false 반환
